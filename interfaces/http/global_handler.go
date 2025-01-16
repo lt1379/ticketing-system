cde9ff9a5df403e2f561fc9419803bb56ba093e4
Lambok Tulus Simamora <lamboktulus1379@gmail.com>
2025-01-16 12:53:39 +0700

    refactor: move validationError to global_handler.go

---
+++ interfaces/http/global_handler.go
@@ -0,0 +1,15 @@
+package http
+
+type validationError struct {
+	Namespace       string `json:"namespace"` // can differ when a custom TagNameFunc is registered or
+	Field           string `json:"field"`     // by passing alt name to ReportError like below
+	StructNamespace string `json:"structNamespace"`
+	StructField     string `json:"structField"`
+	Tag             string `json:"tag"`
+	ActualTag       string `json:"actualTag"`
+	Kind            string `json:"kind"`
+	Type            string `json:"type"`
+	Value           string `json:"value"`
+	Param           string `json:"param"`
+	Message         string `json:"message"`
+}
--- interfaces/http/ticket_handler.go
+++ interfaces/http/ticket_handler.go
@@ -85,20 +85,6 @@ func (t TicketHandler) GetAll(context *gin.Context) {
 	context.JSON(http.StatusOK, res)
 }
 
-type validationError struct {
-	Namespace       string `json:"namespace"` // can differ when a custom TagNameFunc is registered or
-	Field           string `json:"field"`     // by passing alt name to ReportError like below
-	StructNamespace string `json:"structNamespace"`
-	StructField     string `json:"structField"`
-	Tag             string `json:"tag"`
-	ActualTag       string `json:"actualTag"`
-	Kind            string `json:"kind"`
-	Type            string `json:"type"`
-	Value           string `json:"value"`
-	Param           string `json:"param"`
-	Message         string `json:"message"`
-}
-
 func (t TicketHandler) Create(c *gin.Context) {
 	var res dto.Res
 	var req dto.RequestTicketDto
