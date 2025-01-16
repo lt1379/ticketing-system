95035d1ba13b468465bca5c48e4fc0ccba71aa75
Lambok Tulus Simamora <lamboktulus1379@gmail.com>
2025-01-16 13:12:32 +0700

    test: updated util_test.go to test only year

--- infrastructure/utils/util_test.go
+++ infrastructure/utils/util_test.go
@@ -7,18 +7,19 @@ import (
 )
 
 func TestGetCurrentTime(t *testing.T) {
+	now, _, _ := time.Now().UTC().Date()
 	tests := []struct {
 		name string
-		want time.Time
+		want int
 	}{
 		{
 			name: "TestGetCurrentTime - 1",
-			want: time.Now().UTC(),
+			want: now,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := GetCurrentTime(); !reflect.DeepEqual(got, tt.want) {
+			if got, _, _ := GetCurrentTime().Date(); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("GetCurrentTime() = %v, want %v", got, tt.want)
 			}
 		})
